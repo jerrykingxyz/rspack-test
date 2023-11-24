cecb0a9caf4841278a21100dac6742e35f5af066
jerrykingxyz <jerrykingxyz@gmail.com>
2023-11-24 15:06:53 +0800

    feat: add loader

--- rspack.config.js
+++ rspack.config.js
@@ -1,4 +1,19 @@
 /** @type {import("@rspack/cli").Configuration} */
 module.exports = {
   entry: { main: "./src/Three.js" },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        use: [
+          {
+            loader: "./loader.cjs",
+            options: {
+              times: 10,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
